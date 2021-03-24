0bcc7a15c4d091015c54851c1b5847048e71f9ec
yosianindra24 <81297460+yosianindra24@users.noreply.github.com>
2021-03-25 01:16:19 +0700

    Add files via upload

---
+++ chatasix/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ChatAsix</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+	<link rel="icon" type="img/favicon.svg" href="img/favicon.svg">
+	<style type="text/css">
+		font-family: 'Berkshire Swash', cursive;
+		font-family: 'Courier Prime', monospace;
+		font-family: 'Bebas Neue' , cursive;
+		font-family: 'Amatic SC', cursive;
+		font-family: 'Architects Daughter', cursive;
+	</style>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Courier+Prime:wght@700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&family=Architects+Daughter&family=Bebas+Neue&display=swap" rel="stylesheet">
+	<script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+</head>
+<body style="background-image: url('img/home.jpg');background-repeat: no-repeat;background-attachment: fixed;background-size: cover;">
+<nav class="navbar navbar-light bg-light ">
+		<div class="container">
+		  <a href="index.php" class="navbar-brand"  style="font-family: Courier Prime; "><i class="fab fa-contao"></i> ChatAsix </a>
+		  <form class="form-inline">
+		   	<a style="font-family: Courier Prime">by YOST<i class="fab fa-angellist"></i></a> 
+		  </form>
+		</div>
+	</nav>
+
+
+	<!-- Main Content -->
+	<div class="container text-white " style="font-family: Architects Daughter; height: 50vh">
+		<div class="row">
+		<div class="col-md-7"  style="margin-top: 100px">
+			<h2 class="" style="filter: drop-shadow(2px 2px 3px grey);" >
+				Chat Asix <i class="fas fa-american-sign-language-interpreting"></i> <br>
+				Chat with ur best Gank <br>
+			</h2><br><br>
+			<a href="signin.php" 	class="btn btn-lg btn-success" style="background-color: #db4132;border: none;">Sign In</a>
+			&nbsp; Or &nbsp; 
+			<a href="signup.php" 	class="btn btn-lg btn-info" style="background-color: #3292db;border: none">Sign Up</a>
+		</div>
+			<div class="col-md-4 ">
+			<h2 class="text-right" style="filter: drop-shadow(3px 3px 4px grey); font-size: 25pt">
+				<br><br><br><br>#Asix-in Aja!!
+			</h2>
+				
+			</div>
+		</div>
+	</div>
+	<footer class="footer bg-black small text-center text-dark mb-4 fixed-bottom sticky-bottom">
+    	<div style="font-family: Bebas Neue;" class="container">
+    		#AsixinAja <br>
+    		Copyright © ChatAsix 20XX
+    	</div>
+    </footer>
+</body>
+</html>
---
+++ chatasix/inti/init.php
@@ -0,0 +1,9 @@
+<?php 
+
+	require_once 'koneksi.php';
+	require_once 'fungsi.php';
+
+	session_start();
+
+
+ ?>
